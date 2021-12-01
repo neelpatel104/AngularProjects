21ea7a7826c2b819cc56b9a5f19fe42fe64d3d72
Neel Patel <neelpatel104@gmail.com>
2021-11-30 22:27:48 -0500

    Added a responsive nav bar

--- RecipeHut/src/app/app.module.ts
+++ RecipeHut/src/app/app.module.ts
@@ -1,7 +1,10 @@
 // Module Imports
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatButton, MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { NgModule } from '@angular/core';
 
 
@@ -16,8 +19,11 @@ import { AppComponent } from './app.component';
 	],
 	imports: [
 		AppRoutingModule,
+		BrowserAnimationsModule,
 		BrowserModule,
-		FlexLayoutModule
+		FlexLayoutModule,
+		MatButtonModule,
+		MatIconModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
